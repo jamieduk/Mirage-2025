0413408189759a22c95e062d62df77ec6cf19952
Jay <jamied_uk@hotmail.com>
2025-09-06 09:19:47 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import mirage
+
+setup(
+
+    name='mirage',
+    version=mirage.__version__,
+    packages=find_packages(),
+    author="Romain Cayre",
+    author_email="rcayre@laas.fr",
+    description="Mirage is an offensive framework dedicated to the security analysis of wireless communication protocols",
+    install_requires=["keyboard","terminaltables","pyusb","pyserial","pycryptodomex","psutil","scapy","matplotlib"],
+    include_package_data=True,
+    url='https://github.com/RCayre/mirage',
+
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: French",
+        "Operating System :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Security",
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'mirage=mirage.mirage:main',
+        ],
+    },
+)
---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# (c) J~Net 2025
+#
+python -m venv venv
+source venv/bin/activate
+echo "Virtual Environment Setup and ready!"
+
+unzip -o mirage.zip -d .
+
